616951336984b61df582c1223ee59bb01603beb6
Dhruv Yadav <dhruv1103@gmail.com>
2026-03-14 20:47:53 +0530

    fix: import BillingModule in ConnectionsModule for StripeService injection

--- apps/api/src/connections/connections.module.ts
+++ apps/api/src/connections/connections.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConnectionsController } from './connections.controller';
 import { AuthModule } from '../auth/auth.module';
+import { BillingModule } from '../billing/billing.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, BillingModule],
   controllers: [ConnectionsController],
 })
 export class ConnectionsModule {}
